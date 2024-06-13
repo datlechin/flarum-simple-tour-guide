02019bb1c85e36dab876456bd305fec896f22bec
Ngo Quoc Dat <datlechin@gmail.com>
2024-06-14 01:32:25 +0700

    wip

--- js/src/admin/components/SettingsPage.tsx
+++ js/src/admin/components/SettingsPage.tsx
@@ -69,10 +69,11 @@ export default class SettingsPage extends ExtensionPage {
                               onclick={() => app.modal.show(EditTourGuideStepModal, { model: step })}
                             />
                           </td>
-                          <td>{step.isTriggerClick()
-                            ? app.translator.trans('datlechin-simple-tour-guide.admin.yes')
-                            : app.translator.trans('datlechin-simple-tour-guide.admin.no')
-                          }</td>
+                          <td>
+                            {step.isTriggerClick()
+                              ? app.translator.trans('datlechin-simple-tour-guide.admin.yes')
+                              : app.translator.trans('datlechin-simple-tour-guide.admin.no')}
+                          </td>
                         </tr>
                       ))}
                     </tbody>
